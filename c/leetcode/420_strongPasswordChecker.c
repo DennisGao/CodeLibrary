79634c7f24a9ac1ba7edf26ce43255db1cc50b39
gaohongda <gao.hongda@gmail.com>
2018-11-07 18:29:54 +0800

    ..

--- c/leetcode/420_strongPasswordChecker.c
+++ c/leetcode/420_strongPasswordChecker.c
@@ -57,19 +57,52 @@ void checkCharactor(char b, int* cflag)
 	}
 }
 
+int checkRepChar(char *b, int slen)
+{
+	char *ptr = b;
+	int repCharCnt = 0;
+	slen -= 2;
+	while (slen > 0 ) {
+		if (( *ptr >= 'a' && *ptr <= 'z')
+		|| ( *ptr >= 'A' && *ptr <= 'Z')
+		|| ( *ptr >= '0' && *ptr <= '9')) {
+			if (*ptr==*(ptr+1) && *ptr==*(ptr+2)) {
+				repCharCnt++;
+				ptr += 3;
+				slen -= 3;
+			}
+			else {
+				ptr++;
+				slen--;
+			}
+		}
+		else {
+			ptr++;
+			slen--;
+			}
+	}
+	return repCharCnt;
+}
+
 int strongPasswordChecker(char* s) {
 	int i = 0;
 	int slen = strlen(s);
 	int charisfull = 3;	// 大小写、数字缺少的个数
 	int lenbet = 0;
 	int cflag = 0;
+	int repcnt = 0;
 
 	// 获取长度差
 	lenbet = checkLens(slen);
 
+	// 获取重复子串数量
+	if (slen > 2)
+		repcnt = checkRepChar(s, slen);
+
 	// 0. 长度不足，且至少需要补足3个的，直接返回缺少的长度
-	if (lenbet <= -3)
-		return -lenbet;
+	//if (lenbet <= -3)
+	if (lenbet <0)
+		return repcnt-lenbet;
 
 
 	// 记录字符大小写、数字的情况
@@ -92,34 +125,50 @@ int strongPasswordChecker(char* s) {
 			charisfull--;
 	}
 
+
 	// 分情况判断最终结果
 	// 1. 比规定要长
-	if (lenbet > 0)
-		return lenbet + charisfull;
-	
+	if (lenbet > 0) {
+		if (repcnt*3 < lenbet)
+			return lenbet + charisfull;
+		else
+		{
+			int tmp = (repcnt*3-lenbet)/3+1;
+			int tmp2 = lenbet + charisfull;
+			return tmp > tmp2 ? tmp:tmp2;
+		}
+	}
 	
 	// 2. 比规定短
 	if (lenbet < 0)
-		return abs(lenbet)<charisfull ? charisfull: abs(lenbet);
+		return (abs(lenbet)<charisfull ? charisfull: abs(lenbet))+repcnt;
 
 	// 3. 在规定长度内
-	return charisfull;
+	return charisfull>repcnt ? charisfull : repcnt;
 }
 
 int main(int argc, char** argv)
 {
-	printf("%d\n", strongPasswordChecker(""));	// 6
-	printf("%d\n", strongPasswordChecker("aaa111"));	// 2
+//	printf("%d\n", strongPasswordChecker(""));	// 6
+//	printf("%d\n", strongPasswordChecker("..."));	// 3
+//	printf("%d\n", strongPasswordChecker("a"));	// 5
+//	printf("%d\n", strongPasswordChecker("aaaaa"));	// 2
+//	printf("%d\n", strongPasswordChecker("aaa111"));	// 2
+//	printf("%d\n", strongPasswordChecker("Aa1aclsdwegsadkjfasgAAAAAAA"));	// 7
+//	
+//	printf("%d\n", strongPasswordChecker("Ab1acc"));	// 0
+//	printf("%d\n", strongPasswordChecker("Abaacc"));	// 1
+//	printf("%d\n", strongPasswordChecker("abaacc"));	// 2
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaa"));	// 6
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaa"));	// 6
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaA"));	// 6
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaa3A"));	// 6
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaa"));	// 5
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaA"));	// 5
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaa3A"));	// 5
+//	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaaa"));	// 7
 	
-	printf("%d\n", strongPasswordChecker("Ab1acc"));	// 0
-	printf("%d\n", strongPasswordChecker("Abaacc"));	// 1
-	printf("%d\n", strongPasswordChecker("abaacc"));	// 2
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaa"));	// 2
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaA"));	// 1
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaa3A"));	// 0
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaa"));	// 2
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaA"));	// 1
-	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaa3A"));	// 0
+	printf("%d\n", strongPasswordChecker("1234567890123456Baaaaa"));	// 3
 
 //	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaaa"));	// 3
 //	printf("%d\n", strongPasswordChecker("aaaaaaaaaaaaaaaaaaaaA"));	// 2
